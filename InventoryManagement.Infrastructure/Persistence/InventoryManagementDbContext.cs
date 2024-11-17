2f6c774ec2078f58b2a9b9ad27a1a16b3ff618df
Henrique carvalho <henriqueart7@gmail.com>
2024-11-17 14:14:48 -0300

    correção da table Customers

--- InventoryManagement.Infrastructure/Persistence/InventoryManagementDbContext.cs
+++ InventoryManagement.Infrastructure/Persistence/InventoryManagementDbContext.cs
@@ -11,7 +11,7 @@ namespace InventoryManagement.Infrastructure.Persistence
         }
 
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Customer> customers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Sale> Sales { get; set; }
         public DbSet<SaleItem> SaleItems { get; set; }
