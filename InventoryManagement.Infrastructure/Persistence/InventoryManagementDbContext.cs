41c57bf779f2478d19e685cb41b595cc4e2babee
Henrique carvalho <henriqueart7@gmail.com>
2024-11-17 15:03:25 -0300

    adicionando configurações as tabelas

--- InventoryManagement.Infrastructure/Persistence/InventoryManagementDbContext.cs
+++ InventoryManagement.Infrastructure/Persistence/InventoryManagementDbContext.cs
@@ -7,7 +7,7 @@ namespace InventoryManagement.Infrastructure.Persistence
     {
         public InventoryManagementDbContext(DbContextOptions<InventoryManagementDbContext> options) : base(options)
         {
-            
+
         }
 
         public DbSet<Category> Categories { get; set; }
@@ -19,7 +19,58 @@ namespace InventoryManagement.Infrastructure.Persistence
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Configuração para Category
+            modelBuilder.Entity<Category>()
+                .HasKey(u => u.Id);
+
+            modelBuilder.Entity<Category>()
+                .Property(i => i.Name)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            modelBuilder.Entity<Category>()
+                .Property(i => i.Description)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            // Configuração para Customer
+            modelBuilder.Entity<Customer>()
+                .HasKey(u => u.Id);
+
+            modelBuilder.Entity<Customer>()
+                .Property(i => i.Name)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            modelBuilder.Entity<Customer>()
+                .Property(i => i.Email)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            modelBuilder.Entity<Customer>()
+                .Property(i => i.Phone)
+                .IsRequired()
+                .HasMaxLength(15);
+
             // Configuração para Product
+            modelBuilder.Entity<Product>()
+                .HasKey(u => u.Id);
+
+            modelBuilder.Entity<Product>()
+                .Property(i => i.Price)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<Product>()
+                .Property(i => i.Description)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            modelBuilder.Entity<Product>()
+                .Property(i => i.Name)
+                .IsRequired()
+                .HasMaxLength(150);
+
             modelBuilder.Entity<Product>()
                 .HasOne(p => p.Category)
                 .WithMany(c => c.Products)
@@ -34,6 +85,14 @@ namespace InventoryManagement.Infrastructure.Persistence
 
             // Configuração para SaleItem
             modelBuilder.Entity<SaleItem>()
+                .HasKey(u => u.Id);
+
+            modelBuilder.Entity<SaleItem>()
+                .Property(i => i.UnitPrice)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<SaleItem>()
                 .HasOne(si => si.Sale)
                 .WithMany(s => s.SaleItems)
                 .HasForeignKey(si => si.IdSale);
@@ -46,10 +105,42 @@ namespace InventoryManagement.Infrastructure.Persistence
 
             // Configuração para Sale
             modelBuilder.Entity<Sale>()
+                .HasKey(u => u.Id);
+
+            modelBuilder.Entity<Sale>()
+                .Property(i => i.TotalAmount)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<Sale>()
                 .HasOne(s => s.Customer)
                 .WithMany(c => c.Sales)
                 .HasForeignKey(s => s.IdCustomer)
                 .OnDelete(DeleteBehavior.Restrict);
+
+            // Configuração para Supplier
+            modelBuilder.Entity<Supplier>()
+                .HasKey(u => u.Id);
+
+            modelBuilder.Entity<Supplier>()
+                .Property(i => i.Name)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            modelBuilder.Entity<Supplier>()
+                .Property(i => i.Email)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            modelBuilder.Entity<Supplier>()
+                .Property(i => i.Address)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            modelBuilder.Entity<Supplier>()
+                .Property(i => i.Contact)
+                .IsRequired()
+                .HasMaxLength(15);
         }
     }
 }
