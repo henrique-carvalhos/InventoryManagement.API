73d7c671196e9a64efc357b1640e4e5df1619e69
Henrique carvalho <henriqueart7@gmail.com>
2024-11-17 13:54:46 -0300

    configurando InventoryManagementDbContext

---
+++ InventoryManagement.Infrastructure/Persistence/InventoryManagementDbContext.cs
@@ -0,0 +1,20 @@
+﻿using InventoryManagement.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Infrastructure.Persistence
+{
+    public class InventoryManagementDbContext : DbContext
+    {
+        public InventoryManagementDbContext(DbContextOptions<InventoryManagementDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<SaleItem> SaleItems { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
