7d0275095d1f0e4295502380284cc67e4ff80014
Henrique carvalho <henriqueart7@gmail.com>
2024-11-19 19:22:35 -0300

    implementando DeleteCustomerCommand

---
+++ InventoryManagement.Application/Commands/DeleteCustomer/DeleteCustomerCommand.cs
@@ -0,0 +1,15 @@
+﻿using InventoryManagement.Application.Models;
+using MediatR;
+
+namespace InventoryManagement.Application.Commands.DeleteCustomer
+{
+    public class DeleteCustomerCommand : IRequest<ResultViewModel>
+    {
+        public DeleteCustomerCommand(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
---
+++ InventoryManagement.Application/Commands/DeleteCustomer/DeleteCustomerCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using InventoryManagement.Application.Models;
+using InventoryManagement.Core.Repositories;
+using MediatR;
+
+namespace InventoryManagement.Application.Commands.DeleteCustomer
+{
+    public class DeleteCustomerCommandHandler : IRequestHandler<DeleteCustomerCommand, ResultViewModel>
+    {
+        private readonly ICustomerRepository _repository;
+        public DeleteCustomerCommandHandler(ICustomerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ResultViewModel> Handle(DeleteCustomerCommand request, CancellationToken cancellationToken)
+        {
+            var customer = await _repository.GetbyId(request.Id);
+
+            if (customer is null)
+            {
+                return ResultViewModel.Error("Fornecedor não encontrado");
+            }
+
+            customer.SetAsDeleted();
+            await _repository.Update(customer); 
+
+            return ResultViewModel.Success();   
+        }
+    }
+}
