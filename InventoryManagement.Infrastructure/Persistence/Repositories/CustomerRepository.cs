1712e2d5d4f5c3ec373043ef8b878b7974e6ed9c
Henrique carvalho <henriqueart7@gmail.com>
2024-11-19 18:55:48 -0300

    criando interfaca e repositorio do Customer

---
+++ InventoryManagement.Infrastructure/Persistence/Repositories/CustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using InventoryManagement.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Infrastructure.Persistence.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly InventoryManagementDbContext _context;
+        public CustomerRepository(InventoryManagementDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
