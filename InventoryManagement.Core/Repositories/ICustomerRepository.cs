d2a044fbeb04f13ef3faf595be77dad2da7f3439
Henrique carvalho <henriqueart7@gmail.com>
2024-11-19 19:00:18 -0300

    implementando CustomerRepository

--- InventoryManagement.Core/Repositories/ICustomerRepository.cs
+++ InventoryManagement.Core/Repositories/ICustomerRepository.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using InventoryManagement.Core.Entities;
 
 namespace InventoryManagement.Core.Repositories
 {
     public interface ICustomerRepository
     {
-
+        Task<Customer?> GetbyId(int id);
+        Task<List<Customer>> GetAll(string search);
+        Task<int> Add(Customer customer);
+        Task Update(Customer customer);
     }
 }
--- InventoryManagement.Infrastructure/Persistence/Repositories/CustomerRepository.cs
+++ InventoryManagement.Infrastructure/Persistence/Repositories/CustomerRepository.cs
@@ -1,9 +1,6 @@
-﻿using InventoryManagement.Core.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using InventoryManagement.Core.Entities;
+using InventoryManagement.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace InventoryManagement.Infrastructure.Persistence.Repositories
 {
@@ -13,6 +10,35 @@ namespace InventoryManagement.Infrastructure.Persistence.Repositories
         public CustomerRepository(InventoryManagementDbContext context)
         {
             _context = context;
+        }
+
+        public async Task<int> Add(Customer customer)
+        {
+            _context.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return customer.Id;
+        }
+
+        public async Task<List<Customer>> GetAll(string search)
+        {
+            var customers = await _context.Customers
+                .Where(u => !u.IsDeleted && (search == "" || u.Name.Contains(search)))
+                .ToListAsync();
+
+            return customers;
+        }
+
+        public async Task<Customer?> GetbyId(int id)
+        {
+            return await _context.Customers
+                .SingleOrDefaultAsync(customer => customer.Id == id);
+        }
+
+        public async Task Update(Customer customer)
+        {
+            _context.Update(customer);
+            await _context.SaveChangesAsync();
         }
     }
 }
