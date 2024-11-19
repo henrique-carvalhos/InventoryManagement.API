b340f788d3bafe3bb981ed5ccf272cd19c2f2321
Henrique carvalho <henriqueart7@gmail.com>
2024-11-19 19:12:41 -0300

    implementando GetCustomerByIdQuery

---
+++ InventoryManagement.Application/Queries/GetCustomerById/GetCustomerByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using InventoryManagement.Application.Models;
+using MediatR;
+
+namespace InventoryManagement.Application.Queries.GetCustomerById
+{
+    public class GetCustomerByIdQuery : IRequest<ResultViewModel<CustomerViewModel>>
+    {
+        public GetCustomerByIdQuery(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; set; }
+    }
+}
---
+++ InventoryManagement.Application/Queries/GetCustomerById/GetCustomerByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using InventoryManagement.Application.Models;
+using InventoryManagement.Core.Repositories;
+using MediatR;
+
+namespace InventoryManagement.Application.Queries.GetCustomerById
+{
+    public class GetCustomerByIdQueryHandler : IRequestHandler<GetCustomerByIdQuery, ResultViewModel<CustomerViewModel>>
+    {
+        private readonly ICustomerRepository _repository;
+        public GetCustomerByIdQueryHandler(ICustomerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ResultViewModel<CustomerViewModel>> Handle(GetCustomerByIdQuery request, CancellationToken cancellationToken)
+        {
+            var customer = await _repository.GetbyId(request.Id);
+
+            if (customer is null)
+            {
+                return ResultViewModel<CustomerViewModel>.Error("Fornecedor não encontrada");
+            }
+
+            var model = CustomerViewModel.FromEntity(customer);
+
+            return ResultViewModel<CustomerViewModel>.Success(model);
+        }
+    }
+}
