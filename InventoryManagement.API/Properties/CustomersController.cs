93d662d34a51db3f4b9fba1d7524a3ef59bdce0b
Henrique carvalho <henriqueart7@gmail.com>
2024-11-19 18:53:15 -0300

    criando CustomersController

---
+++ InventoryManagement.API/Properties/CustomersController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.API.Properties
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CustomersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
