06e4935ea0e5dc553399126809c1849bf1ff5a4c
Henrique carvalho <henriqueart7@gmail.com>
2024-11-20 20:55:05 -0300

    implementando métodos no ISupplierRepository

--- InventoryManagement.Core/Repositories/ISupplierRepository.cs
+++ InventoryManagement.Core/Repositories/ISupplierRepository.cs
@@ -1,7 +1,12 @@
-﻿namespace InventoryManagement.Core.Repositories
+﻿using InventoryManagement.Core.Entities;
+
+namespace InventoryManagement.Core.Repositories
 {
     public interface ISupplierRepository
     {
-
+        Task<Supplier?> GetbyId(int id);
+        Task<List<Supplier>> GetAll(string search);
+        Task<int> Add(Supplier supplier);
+        Task Update(Supplier supplier);
     }
 }
