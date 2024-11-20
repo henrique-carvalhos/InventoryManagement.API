4b686202564d07814591f20366393bc42c8434ae
Henrique carvalho <henriqueart7@gmail.com>
2024-11-20 20:51:48 -0300

    criando SuppliersController

---
+++ InventoryManagement.API/Controllers/SuppliersController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.API.Controllers
+{
+    [Route("api/suppliers")]
+    [ApiController]
+    public class SuppliersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public SuppliersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+
+    }
+}
