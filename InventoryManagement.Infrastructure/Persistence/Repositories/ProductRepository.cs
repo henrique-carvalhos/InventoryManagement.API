9abf2512a98ac38246a5b0ec882a7c656f9bc236
Henrique carvalho <henriqueart7@gmail.com>
2024-11-22 10:24:36 -0300

    criando ProductRepository

---
+++ InventoryManagement.Infrastructure/Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,33 @@
+﻿using InventoryManagement.Core.Entities;
+using InventoryManagement.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Infrastructure.Persistence.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<int> Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> GetAll(string search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product?> GetbyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
