63df964b03a3ca8f9c77286e9dea6070c5ef0ce0
Henrique carvalho <henriqueart7@gmail.com>
2024-11-22 10:22:10 -0300

    criando ProductsController

---
+++ InventoryManagement.API/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.API.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
