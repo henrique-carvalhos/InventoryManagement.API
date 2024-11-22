a8de6457f0a6b282de1f808826ded29c45ec2319
Henrique carvalho <henriqueart7@gmail.com>
2024-11-22 10:23:47 -0300

    implementando IProductRepository

---
+++ InventoryManagement.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using InventoryManagement.Core.Entities;
+
+namespace InventoryManagement.Core.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<Product?> GetbyId(int id);
+        Task<List<Product>> GetAll(string search);
+        Task<int> Add(Product product);
+        Task Update(Product product);
+    }
+}
