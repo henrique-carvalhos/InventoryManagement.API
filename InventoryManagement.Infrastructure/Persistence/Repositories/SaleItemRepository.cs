2604e238821c1eb0d57e2545fe4ebcbbdfd9f702
Henrique carvalho <henriqueart7@gmail.com>
2024-11-23 14:00:31 -0300

    criando SaleItemRepository

---
+++ InventoryManagement.Infrastructure/Persistence/Repositories/SaleItemRepository.cs
@@ -0,0 +1,33 @@
+﻿using InventoryManagement.Core.Entities;
+using InventoryManagement.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Infrastructure.Persistence.Repositories
+{
+    public class SaleItemRepository : ISaleRepository
+    {
+        public Task<int> Add(Sale sale)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Sale>> GetAll(string search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Sale?> GetbyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Sale sale)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
