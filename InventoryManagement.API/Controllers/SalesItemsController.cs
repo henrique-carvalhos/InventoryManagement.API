6fee14e2a6e97d1dfa06e92916662b1b8f52984d
Henrique carvalho <henriqueart7@gmail.com>
2024-11-23 13:58:14 -0300

    criando SalesItemsController

---
+++ InventoryManagement.API/Controllers/SalesItemsController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.API.Controllers
+{
+    [Route("api/salesItems")]
+    [ApiController]
+    public class SalesItemsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public SalesItemsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
