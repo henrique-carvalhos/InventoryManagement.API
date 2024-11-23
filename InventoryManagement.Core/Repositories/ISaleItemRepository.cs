22e12ca02912e9cd580508eb8fd18db791f0a57a
Henrique carvalho <henriqueart7@gmail.com>
2024-11-23 13:59:51 -0300

    criando ISaleItemRepository

---
+++ InventoryManagement.Core/Repositories/ISaleItemRepository.cs
@@ -0,0 +1,12 @@
+﻿using InventoryManagement.Core.Entities;
+
+namespace InventoryManagement.Core.Repositories
+{
+    public interface ISaleItemRepository
+    {
+        Task<SaleItem?> GetbyId(int id);
+        Task<List<SaleItem>> GetAll(string search);
+        Task<int> Add(SaleItem item);
+        Task Update(SaleItem item);
+    }
+}
