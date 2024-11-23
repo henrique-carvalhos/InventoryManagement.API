b30f0b8c72356e1c3cde9f5ec5f530de5a37e781
Henrique carvalho <henriqueart7@gmail.com>
2024-11-23 10:21:18 -0300

    criando interface ISaleRepository

---
+++ InventoryManagement.Core/Repositories/ISaleRepository.cs
@@ -0,0 +1,12 @@
+﻿using InventoryManagement.Core.Entities;
+
+namespace InventoryManagement.Core.Repositories
+{
+    public interface ISaleRepository
+    {
+        Task<Sale?> GetbyId(int id);
+        Task<List<Sale>> GetAll(string search);
+        Task<int> Add(Sale sale);
+        Task Update(Sale sale);
+    }
+}
