9568fbce12aafbdb0db356513c0d8fade5a88fc7
Henrique carvalho <henriqueart7@gmail.com>
2024-11-24 11:02:24 -0300

    implementando InsertSaleItemValidator

---
+++ InventoryManagement.Application/Validators/InsertSaleItemValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using InventoryManagement.Application.Commands.InsertSaleItem;
+
+namespace InventoryManagement.Application.Validators
+{
+    public class InsertSaleItemValidator : AbstractValidator<InsertSaleItemCommand>
+    {
+        public InsertSaleItemValidator()
+        {
+            RuleFor(p => p.Quantity)
+                .NotEmpty()
+                .WithMessage("Quantidade não pode ser vazio")
+                .GreaterThan(0)
+                    .WithMessage("Quantidade deve ser maior que zero");
+
+            RuleFor(p => p.UnitPrice)
+                .NotEmpty()
+                .WithMessage("Preço unitário não pode ser vazio")
+                .GreaterThan(0)
+                    .WithMessage("Preço unitário deve ser maior R$0,00");
+        }
+    }
+}
