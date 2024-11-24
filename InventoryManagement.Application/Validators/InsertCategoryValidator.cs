4c840f43af3eae19e431285adddb2f5047dbbe7c
Henrique carvalho <henriqueart7@gmail.com>
2024-11-24 10:33:16 -0300

    implementando InsertCategoryValidator

---
+++ InventoryManagement.Application/Validators/InsertCategoryValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using InventoryManagement.Application.Commands.InsertCaregory;
+
+namespace InventoryManagement.Application.Validators
+{
+    public class InsertCategoryValidator : AbstractValidator<InsertCategoryCommand>
+    {
+        public InsertCategoryValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .WithMessage("Descrição não pode ser vazia")
+                .MaximumLength(150)
+                .WithMessage("Tamanho máximo é de 150 caracteres");
+
+            RuleFor(p => p.Description)
+                .NotEmpty()
+                .WithMessage("Descrição não pode ser vazia")
+                .MaximumLength(150)
+                .WithMessage("Tamanho máximo é de 150 caracteres");
+        }
+    }
+}
